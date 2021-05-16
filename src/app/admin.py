2c566d98888f0e4c964f569cf188e32b9785c45f
Nicolas Anelli <nickanelli@hotmail.com>
2021-05-16 20:33:58 -0300

    E5: feat: configurando admin

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Homepage)
+admin.site.register(HomepageLink)
+admin.site.register(Post)
+admin.site.register(Project)
+admin.site.register(ProjectLink)
