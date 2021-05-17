f8d87d7dc42e42c19ff59f528382b10a67845401
Nicolas Anelli <nickanelli@hotmail.com>
2021-05-16 21:35:32 -0300

    E5: feat: criando views

--- src/app/views.py
+++ src/app/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Home page!")
+    return render(request, "index.html")
 
 def blog(request):
-    return HttpResponse("Blog page!")
+    return render(request, "blog_list.html")
 
 def post(request, slug):
-    return HttpResponse(f"Post \"{slug}\" page!")
+    return render(request, "blog_post.html")
 
 def portfolio(request):
-    return HttpResponse("Portfolio page!")
+    return render(request, "portfolio.html")
