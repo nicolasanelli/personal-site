e166f1afa3601ea3c05a734bc86f87647ee6ae79
Nicolas Anelli <nickanelli@hotmail.com>
2021-05-17 20:31:26 -0300

    E5: chore: changing model types and ordering

--- src/app/models.py
+++ src/app/models.py
@@ -5,7 +5,7 @@ class Homepage(models.Model):
   
   name = models.CharField(max_length=255, help_text="Nome exibido na página inicial")
   subtitle = models.CharField(max_length=255, help_text="Subtitulo exibido abaixo do nome", blank=True, null=True)
-  summary = models.CharField(max_length=2000, help_text="Resumo", blank=True, null=True)
+  summary = models.TextField(help_text="Resumo", blank=True, null=True)
   
   background_image = models.CharField(max_length=255, help_text="Imagem do fundo da pagina", blank=True, null=True)
   cover_image = models.CharField(max_length=255, help_text="Imagem do card", blank=True, null=True)
@@ -18,6 +18,9 @@ class Homepage(models.Model):
     return f"{self.name}" 
 
 class HomepageLink(models.Model):
+  class Meta:
+    ordering = ['order']
+
   icon = models.CharField(max_length=255, help_text="Classe do icone do link")
   url = models.CharField(max_length=255, help_text="Url de destino do link")
   alt = models.CharField(max_length=255, help_text="Texto do link")
@@ -29,20 +32,23 @@ class HomepageLink(models.Model):
     return f"{self.order} - {self.alt} ({self.icon})"
 
 class Post(models.Model):
+  class Meta:
+    ordering = ['-data_publicacao']
+
   title = models.CharField(max_length=255, help_text="Título da postagem")
   slug = models.CharField(max_length=255, help_text="Slug")
   data_publicacao = models.DateField(blank=True, null=True)
-  plain_text = models.CharField(max_length=255, help_text="Conteúdo em texto")
-  markdown_text = models.CharField(max_length=255, help_text="Conteúdo em markdown")
+  plain_text = models.TextField(help_text="Conteúdo em texto")
+  markdown_text = models.TextField(help_text="Conteúdo em markdown")
   
   show = models.BooleanField(default=True)
 
   def __str__(self):
-    return f"{self.title} (Show: {self.show})"
+    return f"[{self.data_publicacao}] - {self.title}"
 
 class Project(models.Model):
   name = models.CharField(max_length=255, help_text="Nome do projeto")
-  description = models.CharField(max_length=2000, help_text="Descrição do projeto")
+  description = models.TextField(max_length=2000, help_text="Descrição do projeto")
   image = models.CharField(max_length=255, help_text="Imagem do projeto")
   technologies = models.CharField(max_length=255, help_text="Tecnologias utilizadas", blank=True, null=True)
   
@@ -52,8 +58,11 @@ class Project(models.Model):
     return f"{self.name}"
 
 class ProjectLink(models.Model):
+  class Meta:
+    ordering = ['-project']
+
   name = models.CharField(max_length=255, help_text="Texto do link")
-  icon = models.CharField(max_length=2000, help_text="Classe do icone do link")
+  icon = models.CharField(max_length=255, help_text="Classe do icone do link")
   url = models.CharField(max_length=255, help_text="Url de destino do link")
   
   show = models.BooleanField(default=True)
@@ -61,4 +70,4 @@ class ProjectLink(models.Model):
   project = models.ForeignKey("Project", on_delete=models.CASCADE)
   
   def __str__(self):
-    return f"{self.name} on #{self.project} (Show: {self.show})"
+    return f"[{self.project}] - {self.name} (Enable: {self.show})"
